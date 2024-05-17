eee31ca0ec4798b38fefe0fe633bfbfe054fbe96
clement <clementwanjau@gmail.com>
2024-05-17 08:24:39 +0300

    Provide a basic example pf the crate usage.

---
+++ examples/basic_tree/src/main.rs
@@ -0,0 +1,32 @@
+use tree_ds::prelude::{Node, NodeRemovalStrategy, Tree};
+
+fn main() {
+	let mut tree = Tree::new();
+	let root = tree.add_node(Node::new("Risk", Some(5000)), None);
+	let fixed_income_node = tree.add_node(Node::new("Fixed Income", Some(2000)), Some(root));
+	let equity_node = tree.add_node(Node::new("Equity", Some(3000)), Some(root));
+	let debt_node = tree.add_node(Node::new("Debt", Some(1000)), Some(fixed_income_node));
+	let mutual_funds_node = tree.add_node(Node::new("Mutual Funds", Some(1000)), Some(equity_node));
+	let stocks_node = tree.add_node(Node::new("Stocks", Some(2000)), Some(equity_node));
+	tree.add_node(Node::new("Debt Mutual Funds", Some(500)), Some(debt_node));
+	tree.add_node(Node::new("Equity Mutual Funds", Some(500)), Some(mutual_funds_node));
+	tree.add_node(Node::new("Large Cap Stocks", Some(1000)), Some(stocks_node));
+	tree.add_node(Node::new("Mid Cap Stocks", Some(1000)), Some(stocks_node));
+	tree.add_node(Node::new("Small Cap Stocks", Some(1000)), Some(stocks_node));
+
+	println!("Original Tree");
+	println!("*********************");
+	println!("{}", tree);
+
+
+	tree.remove_node(stocks_node, NodeRemovalStrategy::RemoveNodeAndChildren);
+	println!("After removing Stocks Node");
+	println!("*******************");
+	println!("{}", tree);
+
+
+	let debt_mutual_funds_sub_tree = tree.get_subtree(equity_node, None);
+	println!("Debt Mutual Funds Sub Tree");
+	println!("*********************");
+	println!("{}", debt_mutual_funds_sub_tree);
+}
