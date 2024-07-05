6006f18078ed8003ce8cd5307cb2b41d4a273d11
clement <clementwanjau@gmail.com>
2024-07-05 03:59:11 +0300

    Change the generator. Use `EpochBasedGenerator` for `std` environments and `SimpleGenerator` for `no_std` environments.

--- src/node/auto_id.rs
+++ src/node/auto_id.rs
@@ -1,5 +1,10 @@
+#[cfg(feature = "no_std")]
 use lazy_static::lazy_static;
-use sequential_gen::prelude::{Generator, SimpleGenerator};
+#[cfg(not(feature = "no_std"))]
+use sequential_gen::prelude::EpochBasedGenerator;
+use sequential_gen::prelude::Generator;
+#[cfg(feature = "no_std")]
+use sequential_gen::prelude::SimpleGenerator;
 
 use crate::lib::*;
 #[cfg(feature = "async")]
@@ -8,13 +13,17 @@ use crate::lib::Arc;
 use crate::lib::Rc;
 use crate::node::{_Node, Node};
 
+#[cfg(feature = "no_std")]
 lazy_static! {
     static ref GENERATOR: SimpleGenerator<usize> = SimpleGenerator::new(1usize);
 }
 
+#[cfg(not(feature = "no_std"))]
+pub const GENERATOR: EpochBasedGenerator = EpochBasedGenerator;
+
 impl<Q, T> Node<Q, T>
 where
-    Q: PartialEq + Eq + Clone + From<i32>,
+    Q: PartialEq + Eq + Clone + From<u128>,
     T: PartialEq + Eq + Clone,
 {
     /// Creates a new node with an auto-generated ID.
@@ -36,8 +45,8 @@ where
     /// ```rust
     /// # use tree_ds::prelude::*;
     ///
-    /// let node = Node::<i32, &str>::new_with_auto_id(Some("Harry Doe"));
-    /// let node_2 = Node::<i32, &str>::new_with_auto_id(Some("Jane Doe"));
+    /// let node = Node::<AutomatedId, &str>::new_with_auto_id(Some("Harry Doe"));
+    /// let node_2 = Node::<AutomatedId, &str>::new_with_auto_id(Some("Jane Doe"));
     /// assert_ne!(node.get_node_id(), node_2.get_node_id());
     /// ```
     ///
@@ -46,7 +55,7 @@ where
         #[cfg(not(feature = "async"))]
         {
             Self(Rc::new(RefCell::new(_Node {
-                node_id: Q::from(GENERATOR.generate() as i32),
+                node_id: Q::from(GENERATOR.generate() as u128),
                 value,
                 children: vec![],
                 parent: None,
@@ -55,7 +64,7 @@ where
         #[cfg(feature = "async")]
         {
             Self(Arc::new(RefCell::new(_Node {
-                node_id: Q::from(GENERATOR.generate() as i32),
+                node_id: Q::from(GENERATOR.generate()),
                 value,
                 children: vec![],
                 parent: None,
@@ -66,13 +75,15 @@ where
 
 #[cfg(test)]
 mod tests {
+    use crate::prelude::AutomatedId;
+
     use super::*;
 
     #[cfg(not(feature = "async"))]
     #[test]
     fn test_new_with_auto_id() {
-        let node = Node::<i32, &str>::new_with_auto_id(Some("Harry Doe"));
-        let node_2 = Node::<i32, &str>::new_with_auto_id(Some("Jane Doe"));
+        let node = Node::<AutomatedId, &str>::new_with_auto_id(Some("Harry Doe"));
+        let node_2 = Node::<AutomatedId, &str>::new_with_auto_id(Some("Jane Doe"));
         assert_eq!(node.get_value(), Some("Harry Doe"));
         assert_ne!(node.get_node_id(), node_2.get_node_id());
     }
@@ -80,8 +91,8 @@ mod tests {
     #[cfg(feature = "async")]
     #[test]
     fn test_new_with_auto_id_async() {
-        let node = Node::<i32, &str>::new_with_auto_id(Some("Harry Doe"));
-        let node_2 = Node::<i32, &str>::new_with_auto_id(Some("Jane Doe"));
+        let node = Node::<AutomatedId, &str>::new_with_auto_id(Some("Harry Doe"));
+        let node_2 = Node::<AutomatedId, &str>::new_with_auto_id(Some("Jane Doe"));
         assert_eq!(node.get_value(), Some("Harry Doe"));
         assert_ne!(node.get_node_id(), node_2.get_node_id());
     }
