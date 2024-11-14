5341feb1763f4e339473ccc2d152268ccd002130
teamplayer3 <alex.teampleyer@gmail.com>
2024-11-14 16:53:57 +0100

    add `update_value` function for `Node`

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -287,6 +287,28 @@ where
         self.0.borrow_mut().value = value;
     }
 
+    /// Update the value of the node.
+    ///
+    /// This method updates the value of the node using a closure.
+    ///
+    /// # Arguments
+    ///
+    /// * `modifier` - The closure to use for updating the value.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use tree_ds::prelude::Node;
+    ///
+    /// let node = Node::new(1, Some(2));
+    /// node.update_value(|value| *value = Some(3));
+    /// assert_eq!(node.get_value(), Some(3));
+    /// ```
+    pub fn update_value(&self, modifier: impl FnOnce(&mut Option<T>)) {
+        let mut node = self.0.borrow_mut();
+        modifier(&mut node.value);
+    }
+
     /// Set the parent of the node.
     ///
     /// This method sets the parent of the node.
